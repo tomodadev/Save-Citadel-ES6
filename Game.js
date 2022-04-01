3f1f209778f91016e6986ca1ec4936b2471a2881
ggomaeng <sungwoopark95@gmail.com>
2022-04-01 19:41:39 +0900

    basic flow

--- Game.js
+++ Game.js
@@ -2,12 +2,18 @@ import Screen from "./Screen.js";
 import StartScreen from "./StartScreen.js";
 import charactersJSON from "./constants/characters.js";
 import Character from "./Character.js";
+import ChooseScreen from "./ChooseScreen.js";
+import PlayScreen from "./PlayScreen.js";
 
 let instance;
 
 export default class Game {
+  life = 3;
   characters;
   boss;
+  player;
+  enemy;
+  shuffledCharacters;
 
   constructor() {
     if (instance) {
@@ -16,22 +22,27 @@ export default class Game {
 
     instance = this;
     this.startScreen = new StartScreen("start");
-    this.chooseScreen = new Screen("choose");
-    this.playScreen = new Screen("play");
+    this.chooseScreen = new ChooseScreen("choose");
+    this.playScreen = new PlayScreen("play");
     this.characters = charactersJSON.map((character) => {
-      const { name, episode, gender, image } = character;
-      return new Character(name, episode[0], gender, image);
+      const { id, name, episode, gender, image } = character;
+      return new Character(id, name, episode[0], gender, image);
     });
-    this.boss = [
-      this.characters[74],
-      this.characters[118],
-      this.characters[119],
-    ];
+
+    this.boss = _.remove(
+      this.characters,
+      (char) => char.id === 74 || char.id === 118 || char.id === 119
+    );
   }
 
   start() {
     this.startScreen.show();
     this.chooseScreen.hide();
     this.playScreen.hide();
+  }
+
+  setCurrentPlayer(character) {
+    this.player = character;
+    console.log("current player", this.characters);
   }
 }
